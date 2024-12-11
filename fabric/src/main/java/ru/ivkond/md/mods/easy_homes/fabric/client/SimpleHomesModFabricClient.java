e58ba0b816f48a18dffd18d9b43758ecd5500d2d
ivkond <ivkond@gmail.com>
2024-12-11 22:06:26 +0300

    Rename ExampleModFabricClient to SimpleHomesModFabricClient

--- fabric/src/main/java/ru/ivkond/md/mods/easy_homes/fabric/client/SimpleHomesModFabricClient.java
+++ fabric/src/main/java/ru/ivkond/md/mods/easy_homes/fabric/client/SimpleHomesModFabricClient.java
@@ -2,7 +2,7 @@ package ru.ivkond.md.mods.easy_homes.fabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public final class ExampleModFabricClient implements ClientModInitializer {
+public final class SimpleHomesModFabricClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // This entrypoint is suitable for setting up client-specific logic, such as rendering.
