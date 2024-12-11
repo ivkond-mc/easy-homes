b3e297170aeff41e93466108da764ebecaa9516d
ivkond <ivkond@gmail.com>
2024-12-11 20:06:03 +0300

    No more EventListener

--- neoforge/src/main/java/ru/ivkond/md/mods/easy_homes/neoforge/SimpleHomesModNeoForge.java
+++ neoforge/src/main/java/ru/ivkond/md/mods/easy_homes/neoforge/SimpleHomesModNeoForge.java
@@ -1,10 +1,16 @@
 package ru.ivkond.md.mods.easy_homes.neoforge;
 
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandBuildContext;
+import net.minecraft.commands.CommandSourceStack;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
 import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+import net.neoforged.neoforge.event.RegisterCommandsEvent;
 import ru.ivkond.md.mods.easy_homes.SimpleHomesMod;
 import ru.ivkond.md.mods.easy_homes.neoforge.init.SimpleHomesForgeConfiguration;
 
@@ -16,5 +22,16 @@ public final class SimpleHomesModNeoForge {
 
         container.registerConfig(ModConfig.Type.SERVER, SimpleHomesForgeConfiguration.CONFIG_SPEC);
         container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @EventBusSubscriber
+    public static class EventListener {
+
+        @SubscribeEvent
+        public static void registerCommands(RegisterCommandsEvent event) {
+            CommandDispatcher<CommandSourceStack> dispatcher = event.getDispatcher();
+            CommandBuildContext buildContext = event.getBuildContext();
+            SimpleHomesMod.registerCommands(dispatcher, buildContext);
+        }
     }
 }
